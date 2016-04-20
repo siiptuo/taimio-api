77d0992cc396d4d8595a6aa7f710475511046d55
Tuomas Siipola <siiptuo@kapsi.fi>
2016-04-20 11:10:30 +0300

    Improve test script

--- test.sh
+++ test.sh
@@ -1,17 +1,43 @@
 #!/usr/bin/env bash
 
+set -e
+
 export TIIMA_DBNAME='tiima_test'
 export TIIMA_USERNAME='tuomas'
 export TIIMA_PASSWORD=''
+
+echo ''
+echo '==============='
+echo 'Create database'
+echo '==============='
+echo ''
 
 dropdb --if-exists tiima_test
 createdb tiima_test
 
 vendor/bin/phinx migrate -e testing
 
+echo ''
+echo '=============='
+echo 'Run web server'
+echo '=============='
+echo ''
+
 php -S localhost:9000 index.php &
 
+echo ''
+echo '========='
+echo 'Run tests'
+echo '========='
+echo ''
+
 vendor/bin/behat
+
+echo ''
+echo '===='
+echo 'Exit'
+echo '===='
+echo ''
 
 dropdb tiima_test
 kill %1
